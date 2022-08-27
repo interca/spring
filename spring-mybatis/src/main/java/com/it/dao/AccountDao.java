3e3056127312780d200dec64954bbd40e4af24f6
hongsheng <1634340724@qq.com>
2022-08-27 21:59:12 +0800

    ssm整合mybaties

--- spring-mybatis/src/main/java/com/it/dao/AccountDao.java
+++ spring-mybatis/src/main/java/com/it/dao/AccountDao.java
@@ -1,11 +1,13 @@
 package com.it.dao;
 
 import com.it.domain.Account;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Mapper
 public interface AccountDao {
     @Select("select * from account")
     List<Account> findAll();
