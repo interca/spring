46234f6271fe4eb4ebe256b64cc335aecf44c146
hongsheng <1634340724@qq.com>
2022-08-27 23:00:34 +0800

    ssm整合mybaties

--- spring-mybatis/src/main/java/com/it/config/mybatisConfig.java
+++ spring-mybatis/src/main/java/com/it/config/mybatisConfig.java
@@ -12,7 +12,7 @@ public class mybatisConfig {
     @Bean
     public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws SQLException {
         SqlSessionFactoryBean s=new SqlSessionFactoryBean();
-        s.setTypeAliasesPackage("com.it.domain");
+        //s.setTypeAliasesPackage("com.it.domain");
         //设置数据源对象
         s.setDataSource(dataSource);
         return s;
@@ -22,7 +22,9 @@ public class mybatisConfig {
     public MapperScannerConfigurer mapperScannerConfigurer(){
       MapperScannerConfigurer msc=new MapperScannerConfigurer();
       //映射在哪个包
-        msc.setBasePackage("com.it.dao");
+        msc.setBasePackage("com/it/dao");
       return msc;
     }
+
+
 }
